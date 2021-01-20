1c820696cdebd7934b2e89b9c489fbb8265da3f5
sapayne <sapayne007@gmail.com>
2021-01-20 09:14:11 -0500

    Removed indentation using spaces to tabs

--- ThreadPool.h
+++ ThreadPool.h
@@ -16,141 +16,141 @@
 class Threadpool {
 
 public:
-    Threadpool(unsigned int);
-    bool working()
-    {
-        return tasks.size();
-    }
-    int numOfThreads()
-    {
-        return workers.size();
-    }
-    int changeNumOfThreads(unsigned int Amount);
-    template<class F, class... Args>
-    auto enqueue(F&& f, Args&&... args)->std::future<decltype(f(args...))>;
-    ~Threadpool();
+	Threadpool(unsigned int);
+	bool working()
+	{
+		return tasks.size();
+	}
+	int numOfThreads()
+	{
+		return workers.size();
+	}
+	int changeNumOfThreads(unsigned int Amount);
+	template<class F, class... Args>
+	auto enqueue(F&& f, Args&&... args)->std::future<decltype(f(args...))>;
+	~Threadpool();
 private:
-    std::string output;
-    // need to keep track of threads so we can join them
-    std::vector<std::thread> workers;
-    // the task queue
-    std::queue<std::function<void()>> tasks;
+	std::string output;
+	// need to keep track of threads so we can join them
+	std::vector<std::thread> workers;
+	// the task queue
+	std::queue<std::function<void()>> tasks;
 
-    // synchronization
-    std::mutex queue_mutex;
-    std::condition_variable condition;
-    std::atomic_bool stop;
-    //  used when removing threads from the threadpool
-    std::vector<bool> preciseThreadStop;
-    void addThreads(int amount);
+	// synchronization
+	std::mutex queue_mutex;
+	std::condition_variable condition;
+	std::atomic_bool stop;
+	//  used when removing threads from the threadpool
+	std::vector<bool> preciseThreadStop;
+	void addThreads(int amount);
 };
 
 // the constructor creates as many threads as possible if a value isn't passed
 inline Threadpool::Threadpool(unsigned int threads = std::thread::hardware_concurrency()) : stop(false)
 {
-    threads = std::min(std::max(threads, (unsigned)1), std::thread::hardware_concurrency());
-    addThreads(threads);
+	threads = std::min(std::max(threads, (unsigned)1), std::thread::hardware_concurrency());
+	addThreads(threads);
 }
 
 // add new work item to the pool
 template<class F, class... Args>
 auto Threadpool::enqueue(F&& f, Args&&... args) -> std::future<decltype(f(args...))>
 {
-    using return_type = decltype(f(args...));
+	using return_type = decltype(f(args...));
 
-    auto task = std::make_shared< std::packaged_task<return_type()> >(
-        std::bind(std::forward<F>(f), std::forward<Args>(args)...)
-        );
+	auto task = std::make_shared< std::packaged_task<return_type()> >(
+		std::bind(std::forward<F>(f), std::forward<Args>(args)...)
+		);
 
-    std::future<return_type> res = task->get_future();
-    {
-        std::unique_lock<std::mutex> lock(queue_mutex);
+	std::future<return_type> res = task->get_future();
+	{
+		std::unique_lock<std::mutex> lock(queue_mutex);
 
-        // don't allow enqueueing after stopping the pool
-        if (stop)
-            throw std::runtime_error("enqueue on stopped ThreadPool");
+		// don't allow enqueueing after stopping the pool
+		if (stop)
+			throw std::runtime_error("enqueue on stopped ThreadPool");
 
-        tasks.emplace([task]() { (*task)(); });
-    }
-    condition.notify_one();
-    return res;
+		tasks.emplace([task]() { (*task)(); });
+	}
+	condition.notify_one();
+	return res;
 }
 
 inline void Threadpool::addThreads(int amount)
 {
-    unsigned int startingIndex = workers.size(), endingIndex = startingIndex + amount;
-    for (unsigned int i = startingIndex; i < endingIndex; i++)
-    {
-        preciseThreadStop.emplace_back(false);
-        workers.emplace_back(
-            [this, i]
-            {
-                while (true)
-                {
-                    std::function<void()> task;
-                    {
-                        std::unique_lock<std::mutex> lock(this->queue_mutex);
-                        this->condition.wait(lock,
-                            [this, i] {
-                                return this->stop || !this->tasks.empty() || this->preciseThreadStop[i];
-                            });
-                        if ((this->stop && this->tasks.empty()) || this->preciseThreadStop[i]) return;
-                        task = std::move(this->tasks.front());
-                        this->tasks.pop();
-                    }
-                    task();
-                }
-            }
-        );
-    }
+	unsigned int startingIndex = workers.size(), endingIndex = startingIndex + amount;
+	for (unsigned int i = startingIndex; i < endingIndex; i++)
+	{
+		preciseThreadStop.emplace_back(false);
+		workers.emplace_back(
+			[this, i]
+			{
+				while (true)
+				{
+					std::function<void()> task;
+					{
+						std::unique_lock<std::mutex> lock(this->queue_mutex);
+						this->condition.wait(lock,
+							[this, i] {
+								return this->stop || !this->tasks.empty() || this->preciseThreadStop[i];
+							});
+						if ((this->stop && this->tasks.empty()) || this->preciseThreadStop[i]) return;
+						task = std::move(this->tasks.front());
+						this->tasks.pop();
+					}
+					task();
+				}
+			}
+		);
+	}
 }
 
 inline int Threadpool::changeNumOfThreads(unsigned int Amount)
 {
-    /*  keeps the Threadpool from allocating more threads than what the cpu can handle, so that the program 
-        doesn't have to suffer from context switching given it is the only program running, and from having less than 1 thread.
-    */
-    int diff = std::min(std::max(Amount, (unsigned)1), std::thread::hardware_concurrency()) - workers.size();
-    std::cout << "Changing number of threads from " << workers.size() << " to " << Amount << std::endl;
+	/*  keeps the Threadpool from allocating more threads than what the cpu can handle, so that the program 
+		doesn't have to suffer from context switching given it is the only program running, and from having less than 1 thread.
+	*/
+	int diff = std::min(std::max(Amount, (unsigned)1), std::thread::hardware_concurrency()) - workers.size();
+	std::cout << "Changing number of threads from " << workers.size() << " to " << Amount << std::endl;
 
-    if (diff > 0)
-    {
-        addThreads(diff);
-    }
-    else if (diff < 0)
-    {
-        int endingIndex = abs(diff);
-        //  removes the first positive diff number of threads starting at index 0
-        for (int i = 0; i < endingIndex; i++)
-        {
-            preciseThreadStop[i] = true;
-            /*  call notify_all multiple times or have another loop to join and delete the workers of the corresponding indices, 
-                not sure which impacts the program running time more
-            */
-            condition.notify_all();
-            workers[i].join();
-            workers[i].~thread();
-        }
-        //  deallocate the old memory, as the joined threads can no longer except any tasks after joining.
-        workers.erase(workers.begin(), workers.begin() + endingIndex);
-        preciseThreadStop.erase(preciseThreadStop.begin(), preciseThreadStop.begin() + endingIndex);
-    }
-    std::cout << "Number of threads after change: " << workers.size() << std::endl;
-    return workers.size();
+	if (diff > 0)
+	{
+		addThreads(diff);
+	}
+	else if (diff < 0)
+	{
+		int endingIndex = abs(diff);
+		//  removes the first positive diff number of threads starting at index 0
+		for (int i = 0; i < endingIndex; i++)
+		{
+			preciseThreadStop[i] = true;
+			/*  call notify_all multiple times or have another loop to join and delete the workers of the corresponding indices, 
+				not sure which impacts the program running time more
+			*/
+			condition.notify_all();
+			workers[i].join();
+			workers[i].~thread();
+		}
+		//  deallocate the old memory, as the joined threads can no longer except any tasks after joining.
+		workers.erase(workers.begin(), workers.begin() + endingIndex);
+		preciseThreadStop.erase(preciseThreadStop.begin(), preciseThreadStop.begin() + endingIndex);
+	}
+	std::cout << "Number of threads after change: " << workers.size() << std::endl;
+	return workers.size();
 }
 
 // the destructor joins all threads
 inline Threadpool::~Threadpool()
 {
-    //std::unique_lock<std::mutex> lock(queue_mutex);
-    stop = true;
-    condition.notify_all();
-    for (std::thread& worker : workers) {
-        worker.join();
-        worker.~thread();
-    }
-    workers.~vector();
-    preciseThreadStop.~vector();
+	//std::unique_lock<std::mutex> lock(queue_mutex);
+	stop = true;
+	condition.notify_all();
+	for (std::thread& worker : workers) {
+		worker.join();
+		worker.~thread();
+	}
+	workers.~vector();
+	preciseThreadStop.~vector();
 }
 
 #endif
--- example.cpp
+++ example.cpp
@@ -7,30 +7,30 @@
 
 int exampleFunc(std::string inputString, int i)
 {
-    std::cout << inputString;
-    return i;
+	std::cout << inputString;
+	return i;
 }
 
 int main()
 {
-    
-    Threadpool pool(4);
-    std::vector< std::future<int> > results;
-    std::string input;
-    for(int i = 0; i < 8; i++) {
-        results.emplace_back(
-        //  putting the variables in the [] allows for them to be accessed within the {} for parameter passing
-            pool.enqueue([input, i] { return exampleFunc(input, i); })
-        );
+	
+	Threadpool pool(4);
+	std::vector< std::future<int> > results;
+	std::string input;
+	for(int i = 0; i < 8; i++) {
+		results.emplace_back(
+		//  putting the variables in the [] allows for them to be accessed within the {} for parameter passing
+			pool.enqueue([input, i] { return exampleFunc(input, i); })
+		);
 
-        input = "Hello World " + std::to_string(i) + "\n";
-        //  if you are not storing the results in a vector
-        pool.enqueue([input, i] { exampleFunc(input, i); });
-    }
-    std::cout << std::endl;
-    for(auto& result: results)
-        std::cout << "Task " << result.get() << " finished.\n";
-    std::cout << std::endl;
-    
-    return 0;
+		input = "Hello World " + std::to_string(i) + "\n";
+		//  if you are not storing the results in a vector
+		pool.enqueue([input, i] { exampleFunc(input, i); });
+	}
+	std::cout << std::endl;
+	for(auto& result: results)
+		std::cout << "Task " << result.get() << " finished.\n";
+	std::cout << std::endl;
+	
+	return 0;
 }
